7bf8c3405bb5d0d151719a0ebec0f6072cfee304
Gordon Dent <gordonmdent@gmail.com>
2015-12-07 22:18:15 -0800

    Minor amends to enable HMR whilst ensuring app behaves as expected with existing socket connections

--- server.webpack.js
+++ server.webpack.js
@@ -2,20 +2,23 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var host = 'localhost';
+var appPort = 3000;
+var devServerPort = 3001;
+
 new WebpackDevServer(webpack(config), {
+  contentBase: 'dist/',
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
   proxy: {
-    '*': 'http://localhost:3000'
+    '*': 'http://' + host + ':' + appPort
   },
-  stats: {
-    colors: true
-  }
-}).listen(3001, 'localhost', function (err) {
+  noInfo: true,
+}).listen(devServerPort, host, function (err) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Dev server listening at localhost:3001');
+  console.log('Webpack Dev Server running at ' + host + ':' + devServerPort);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: 'http://localhost:3001/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
