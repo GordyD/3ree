9301174c0f235a017d5cf9f1008f504c7b2397fa
Stedman <stedmanhood@gmail.com>
2016-04-05 10:59:44 -0700

    Create Home component

---
+++ universal/components/Home.js
@@ -0,0 +1,15 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class Home extends Component {
+  static propTypes = {
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>HOME</h1>
+        <p>Welcome to the home page, baby.</p>
+      </div>
+    );
+  }
+}
