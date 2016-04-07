76b25f6be448a99bd803012588a977cb0e335075
Stedman <stedmanhood@gmail.com>
2016-04-06 23:06:00 -0700

    UserScreen to routes.js

--- universal/routes.js
+++ universal/routes.js
@@ -4,11 +4,13 @@ import { Route, IndexRoute } from 'react-router';
 import PulseApp from './containers/PulseApp';
 import MyEvents from './containers/MyEvents';
 import OtherEvents from './containers/OtherEvents';
+import UserScreen from './containers/UserScreen';
 import Home from './components/Home';
 
 export default (
   <Route path='/' component={PulseApp}>
     <IndexRoute components={{home: Home}} />
+    <Route path='users' components={{userScreen: UserScreen}} />
     <Route path='all-events' components={{myEvents: MyEvents, otherEvents: OtherEvents}} />
     <Route path='my-events' components={{myEvents: MyEvents}} />
     <Route path='other-events' components={{otherEvents: OtherEvents}} />
