efb59c08e0f8707bfec658ccfe86078d689895ab
Stedman <stedmanhood@gmail.com>
2016-04-06 22:20:36 -0700

    Add UserScreen container, decorated w/ Redux connect so that it subscribes to store updates

---
+++ universal/containers/UserScreen.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import UserList from '../components/UserList';
+
+import * as PulseActions from '../actions/PulseActions';
+
+class UserScreen extends Component {
+  static propTypes = {
+    userId: React.PropTypes.string,
+    events: React.PropTypes.array
+  };
+
+  render() {
+    return (
+      <UserList users={this.props.users} />
+    );
+  }
+}
+
+/**
+ * Expose "Smart" Component that is subscribed to store updates.
+ * https://github.com/reactjs/react-redux/blob/master/docs/api.md#provider-store
+ */
+export default connect(
+  state => ({
+    users: state.pulseApp.users,
+  }),
+  dispatch => bindActionCreators(PulseActions, dispatch)
+)(UserScreen);
