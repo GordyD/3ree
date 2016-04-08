014d078caa297c0ee25e73da01f05f3515a5a3b5
Stedman <stedmanhood@gmail.com>
2016-04-08 15:46:03 -0700

    UserService methods to edit and delete the user

--- server/api/service/user.js
+++ server/api/service/user.js
@@ -50,3 +50,31 @@ export function addUser(user) {
     });
   });
 }
+
+export function editUser(id, user) {
+  return connect()
+  .then(conn => {
+    user.updated = new Date();
+    user.firstName = xss(user.firstName);
+    user.lastName = xss(user.lastName);
+    user.address1 = xss(user.address1);
+    user.address2 = xss(user.address2);
+    user.city = xss(user.city);
+    user.state = xss(user.state);
+    user.zipcode = xss(user.zipcode);
+    return r
+    .table('users')
+    .get(id).update(user).run(conn)
+    .then(() => user);
+  });
+}
+
+export function deleteUser(id) {
+  return connect()
+  .then(conn => {
+    return r
+    .table('users')
+    .get(id).delete().run(conn)
+    .then(() => ({id: id, deleted: true}));
+  });
+}
