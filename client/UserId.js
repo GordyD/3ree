136b98fc3e1b3b158d4650aeb310fbe80d2ab838
Gordon Dent <gordonmdent@gmail.com>
2017-04-11 18:38:49 -0700

    Correct uuid function usage and ignore eslint errors for one line

--- client/UserId.js
+++ client/UserId.js
@@ -1,9 +1,14 @@
 // This is a tiny UUID generator to replace node-uuid!
 // See: https://gist.github.com/jed/982883
-function uuid(a){
-  /*eslint-disable */
-  return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)
-  /*eslint-enable */
+
+function uuid() {
+  function b(a){
+    /*eslint-disable */
+    return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)
+    /*eslint-enable */
+  }
+
+  return b();
 }
 
 
